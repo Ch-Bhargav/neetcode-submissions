85b70e985a8819d5898a776baa5c9e30caca9676
Bhargav Ch <110449452+Ch-Bhargav@users.noreply.github.com>
2026-04-28 01:49:48 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> num;
+        for(auto ele : nums){
+            num[ele]++;
+        }
+        for(auto ele : num){
+            if(ele.second > 1) return true;
+        }
+        return false;
+    }
+};
