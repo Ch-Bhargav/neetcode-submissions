e371584240d342e50cd037a3a79d778d6120c783
Bhargav Ch <110449452+Ch-Bhargav@users.noreply.github.com>
2026-05-19 16:44:58 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char,char> closeToOpen = {
+            {')','('},
+            {']','['},
+            {'}','{'}
+        };
+
+        for(const auto& ch : s){
+            if(closeToOpen.count(ch)){
+                if(!st.empty() && st.top() == closeToOpen[ch]){
+                    st.pop();
+                }else{
+                    return false;
+                }
+            }else{
+                st.push(ch);
+            }
+        }
+
+        return st.empty();
+
+    }
+};
